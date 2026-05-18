92b6c29a8d1028ce22515d505f92228b79f103c9
Rayan <164565723+Rayan1105@users.noreply.github.com>
2026-05-18 21:07:27 +0500

    Add files via upload

---
+++ final-lab/models/Video.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  rating: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  thumbnail: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Video', videoSchema);
